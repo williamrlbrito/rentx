ce7ec79491560959368f102bb5dccb5a559d1bc5
William Brito <williamrldb@gmail.com>
2021-11-03 23:06:24 -0400

    --Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
+import { Car } from '../../components/Car';
+
 import {
   Container,
   Header,
@@ -12,6 +14,25 @@ import {
 } from './styles';
 
 export function Home() {
+  const CarDataOne = {
+    brand: 'audi',
+    name: 'RS 5 Coupe',
+    rent: {
+      period: 'Ao Dia',
+      price: 120,
+    },
+    thumbnail: 'https://cdn.sitewebmotors.com.br/uploads/userGallery/5fcfe53240728.png',
+  }
+
+  const CarDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao Dia',
+      price: 340,
+    },
+    thumbnail: 'https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png',
+  }
   return (
     <Container>
       <StatusBar 
@@ -31,6 +52,9 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={CarDataOne}/>
+      <Car data={CarDataTwo}/>
     </Container>
   );
 }
