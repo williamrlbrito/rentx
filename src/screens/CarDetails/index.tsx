666b08423e45961c218876c6b1c97ba3fd2995bb
William Brito <williamrldb@gmail.com>
2021-11-09 13:21:28 -0400

    --Componente de Botão

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
 
 import speedSvg from '../../assets/speed.svg';
 import accelerationSvg from '../../assets/acceleration.svg';
@@ -25,6 +26,7 @@ import {
   Price,
   Abaut,
   Acessories,
+  Footer,
 } from './styles';
 
 export function CarDetails() {
@@ -66,6 +68,10 @@ export function CarDetails() {
           E um belissimno carro para quem gosta de acelerar.
         </Abaut>
       </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
     </Container>
   );
 }
