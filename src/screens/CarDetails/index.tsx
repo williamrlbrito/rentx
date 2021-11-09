cbc51aa2aac6cfc4849f6d210381919b758049a1
William Brito <williamrldb@gmail.com>
2021-11-09 09:11:16 -0400

    --Continuando a interface de Detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -6,6 +6,15 @@ import {
   Container,
   Header,
   CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Abaut,
 } from './styles';
 
 export function CarDetails() {
@@ -18,6 +27,26 @@ export function CarDetails() {
       <CarImages >
         <ImageSlider imagesUrl={['https://cdn.sitewebmotors.com.br/uploads/userGallery/5fcfe53240728.png']} />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 50</Price>
+          </Rent>
+        </Details>
+
+        <Abaut>
+          Este e automovel desportivo. Surgiu do lendario
+          touro de lide indultado napraca Real Maestranza de Sevilla.
+          E um belissimno carro para quem gosta de acelerar.
+        </Abaut>
+      </Content>
     </Container>
   );
 }
