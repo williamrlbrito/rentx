da449660e6aced50500a852b1158d7a37dcbdfad
William Brito <williamrldb@gmail.com>
2021-11-09 14:02:11 -0400

    --Interface de Agendamento

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+import { Button } from '../../components/Button';
+
+export function Scheduling() {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <StatusBar 
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton 
+          onPress={() => {}} 
+          color={theme.colors.shape}
+        />
+
+        <Title>
+          Escolha uma {'\n'}
+          data de inicio e {'\n'}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>
+              09/11/2021
+            </DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATE</DateTitle>
+            <DateValue selected={false}>
+            09/11/2021
+            </DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
