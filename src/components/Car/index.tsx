2131e036a46bfb6154a6a95b281b4d6f812769ab
William Brito <williamrldb@gmail.com>
2021-11-15 14:20:00 -0400

    --Exibindo Icones Dinamicamente

--- src/components/Car/index.tsx
+++ src/components/Car/index.tsx
@@ -3,6 +3,7 @@ import { RectButtonProps } from 'react-native-gesture-handler';
 
 import GasolineSvg from '../../assets/gasoline.svg'
 import { CarDTO } from '../../dtos/CarDTO';
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
 
 import {
   Container,
@@ -25,6 +26,8 @@ export function Car({
   data, 
   ...rest
 }: Props) {
+  const MotorIcon = getAccessoryIcon(data.fuel_type);
+
   return (
     <Container {...rest}>
       <Details>
@@ -38,7 +41,7 @@ export function Car({
           </Rent>
 
           <Type>
-            <GasolineSvg />
+            <MotorIcon />
           </Type>
         </About>
       </Details>
