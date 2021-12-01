cec03c60cda0abcaa4ab97a1de4f2890c68127dc
William Brito <williamrldb@gmail.com>
2021-12-01 08:00:43 -0400

    --Primeira Animação

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import { Button, StyleSheet } from 'react-native';
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
 
 import {
   Container,
 } from './styles';
 
 export function Splash() {
+  const animation = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: animation.value,
+        },
+      ],
+    };
+  });
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * 100;
+  }
+
   return (
     <Container>
+      <Animated.View style={[styles.box, animatedStyle]} />
 
+      <Button title="Go to Home" onPress={handleAnimationPosition} />
     </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  }
+});
