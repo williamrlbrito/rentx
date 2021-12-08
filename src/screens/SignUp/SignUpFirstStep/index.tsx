8bb71c7c65accd7187523d4dc97222c632109d69
William Brito <williamrldb@gmail.com>
2021-12-08 14:22:03 -0400

    --Primeiro Step

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { TouchableWithoutFeedback, KeyboardAvoidingView, Keyboard } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
+import { Input } from '../../../components/Input';
+import { Button } from '../../../components/Button';
 
 import {
   Container,
@@ -22,27 +25,47 @@ export function SignUpFirstStep() {
   }
 
   return (
-    <Container>
-      <Header>
-        <BackButton onPress={handleNavigateBack} />
-        <StepsContainer>
-          <Bullet active />
-          <Bullet />
-        </StepsContainer>
-      </Header>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleNavigateBack} />
+            <StepsContainer>
+              <Bullet active />
+              <Bullet />
+            </StepsContainer>
+          </Header>
 
-      <Title>
-        Crie sua{'\n'}conta
-      </Title>
+          <Title>
+            Crie sua{'\n'}conta
+          </Title>
 
-      <Subtitle>
-        faça seu cadastro de{'\n'}
-        forma rápida e fácil
-      </Subtitle>
+          <Subtitle>
+            faça seu cadastro de{'\n'}
+            forma rápida e fácil
+          </Subtitle>
 
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input
+              iconName="user"
+              placeholder="Nome"
+            />
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+            />
+            <Input
+              iconName="credit-card"
+              placeholder="CNH"
+            />
+          </Form>
+
+          <Button
+            title="Próximo"
+          />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
